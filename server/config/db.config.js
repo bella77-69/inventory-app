86fc152ca2a77551e2127b63b44c56e1663dcad4
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-19 08:25:54 -0700

    test

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const dbConn = mysql.createConnection({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'be61f38c636716',
-    password: '8a71952f',
-    database: 'heroku_02a34e026fc2413'
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'inventory-stock-status'
 });
 dbConn.connect(function(error) {
     if (error) throw error;
@@ -13,4 +13,12 @@ dbConn.connect(function(error) {
 
 module.exports = dbConn;
 
+
+
+// const dbConn = mysql.createConnection({
+//     host: 'us-cdbr-east-05.cleardb.net',
+//     user: 'be61f38c636716',
+//     password: '8a71952f',
+//     database: 'heroku_02a34e026fc2413'
+// });
 
