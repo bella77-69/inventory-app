bf3ff988c85601e3ded274c76b1479751e7ae400
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-19 19:40:09 -0700

    removed build folder in client

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql');
 
 const dbConn = mysql.createConnection({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'be61f38c636716',
-    password: '8a71952f',
-    database: 'heroku_02a34e026fc2413'
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'inventory-stock-status'
+    // host: 'us-cdbr-east-05.cleardb.net',
+    // user: 'be61f38c636716',
+    // password: '8a71952f',
+    // database: 'heroku_02a34e026fc2413'
 });
 dbConn.connect(function(error) {
     if (error) throw error;
