3efc4557453efc229e9093c37612920a3d1c0f8b
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-25 20:56:01 -0700

    test

--- server.js
+++ server.js
@@ -3,35 +3,37 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-//import inventory/admin routes
 const inventoryRoutes = require("./routes/inventory.route");
 const adminRoutes = require("./routes/admin.route");
-// const mysql = require("mysql");
-const path = require("path");
-app.use(cors());
+const path = require('path');
+
 app.use(express.json());
+app.use(cors());
+//parse request data content
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //set up server port
 const PORT = process.env.PORT || 5000;
 
-// ... other app.use middleware
-app.use(express.static(path.join(__dirname, "client", "build")));
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server connected to port: ${PORT}`);
 });
 
-//parse request data content
-app.use(bodyParser.urlencoded({ extended: true }));
-
 //define root route
 app.get("/", (req, res) => {
   res.send("Welcome to Inventory App API");
 });
 
+
 //create inventory/admin routs
 app.use("/api/inventory", inventoryRoutes);
 app.use("/api/inventory/admin", adminRoutes);
+
+
+
