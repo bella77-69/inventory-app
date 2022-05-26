d35704a228b0d62b654e6c9fe75972ea79a1983d
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-25 19:45:59 -0700

    fix: server.js define root route

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const mysql = require("mysql");
-const path = require('path');
-//set up server port
-const PORT = process.env.PORT || 5000;
-
+//import inventory/admin routes
+const inventoryRoutes = require("./routes/inventory.route");
+const adminRoutes = require("./routes/admin.route");
+// const mysql = require("mysql");
+const path = require("path");
 app.use(cors());
 app.use(express.json());
 
+//set up server port
+const PORT = process.env.PORT || 5000;
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client"));
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server connected to port: ${PORT}`);
+});
+
 //parse request data content
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.use(express.static(path.join(__dirname, "client", "build")))
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 //define root route
 app.get("/", (req, res) => {
   res.send("Welcome to Inventory App API");
 });
 
-//import inventory/admin routes
-const inventoryRoutes = require("./routes/inventory.route");
-const adminRoutes = require("./routes/admin.route");
 //create inventory/admin routs
 app.use("/api/inventory", inventoryRoutes);
 app.use("/api/inventory/admin", adminRoutes);
-
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server connected to port: ${PORT}`);
-});
-
