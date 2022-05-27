97812f4a92b5181652af10aa79ac0852bd838b4b
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-26 19:06:41 -0700

    test

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build"));
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`)
 })
