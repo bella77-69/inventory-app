4463a6c1c7fc79700c6e1946aaf37c17748d767c
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-26 19:48:22 -0700

    test

--- server.js
+++ server.js
@@ -36,10 +36,12 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build"));
+// });
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`)
 })
