4054a3d27e1554afca1631217f08044787b632e5
chantelle mckenzie <mrspasceri@hotmail.com>
2022-05-26 18:10:06 -0700

    test

--- server.js
+++ server.js
@@ -27,10 +27,14 @@ const PORT = process.env.PORT || 5000;
 //   res.send("Welcome to Inventory App API");
 // })
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'))
+// }
 
+app.use(express.static('client/build'));
+app.get('*', (req, res) =>{
+    res.sendfile(path.resolve(__dirname + "../client/build/index.html"));
+});
 app.listen(PORT, (req, res) => {
   console.log(`Server connected to port: ${PORT}`)
 })
